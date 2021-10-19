d2a6dc25b53d7aaea9233fec2dc7b067e538ba5f
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2021-10-19 18:15:13 +0100

    Added stubs for orderbook functions

---
+++ lib/OrderBook/OrderBook.ex
@@ -0,0 +1,34 @@
+defmodule OrderBook do
+  @moduledoc """
+  Documentation for OrderBook module.
+  """
+
+  @doc """
+    OrderBook module maintains bid and ask lists as sorted trees.
+  """
+    
+    def best_bid(book) do
+        nil
+    end
+
+    def best_ask(book) do
+        nil
+    end
+
+    def apply_delta(book, delta) do
+        nil
+    end
+
+    def apply_snapshot(book, snapshot) do
+        nil
+    end
+
+    def get_asks(book) do
+        nil
+    end
+
+    def get_bids(book) do
+        nil
+    end
+
+end
