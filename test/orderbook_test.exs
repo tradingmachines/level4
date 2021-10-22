be282500e44b80a7dffebbf25829e8a00ef4048c
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2021-10-22 17:02:17 +0100

    Added all tests. Remodelled bid/ask random data.
    - Moved random delta sampling into own function
    - Adjusted structure of test states to have seporate bids/asks samples
    - Filtered duplicate price levels out of sample bids/asks as multiple entries for same price cannot occur
    - Added global ranges for both bids and asks (individually)

--- test/orderbook_test.exs
+++ test/orderbook_test.exs
@@ -21,22 +21,32 @@ defmodule OrderBookTest do
   use ExUnit.Case
   doctest OrderBook
 
-  @num_price_levels 200
-  @price_range 1000..1500
+  @num_price_levels 100
+  @ask_range 1001..2000
+  @bid_range 0..999
   @volume_range 1..50
 
-  # not setup_all because by default mix will randomize the order in which tests
-  # are executed. sharing and modifying the state of the book in this way is
-  # confusing and might cause some tests to fail intermittently?
+  def rand_sample(n, range) do
+    for(_ <- 1..n, do: Enum.random(range))
+    |> Enum.uniq
+  end
+
+  # not setup_all because by default mix will randomize the order in which tests are executed
+  # as such each test will have a fresh set of random sample deltas
   setup do
     {:ok, book} = OrderBook.start_link()
 
     [
       book: book,
-      data:
+      bids:
         List.zip([
-          for(_ <- 1..@num_price_levels, do: Enum.random(@price_range)),
-          for(_ <- 1..@num_price_levels, do: Enum.random(@volume_range))
+          rand_sample(@num_price_levels, @bid_range),
+          rand_sample(@num_price_levels, @volume_range)
+        ]),
+      asks:
+        List.zip([
+          rand_sample(@num_price_levels, @ask_range),
+          rand_sample(@num_price_levels, @volume_range)
         ])
     ]
   end
@@ -56,14 +66,53 @@ defmodule OrderBookTest do
   # stay the same. more representative. the intersection of the bid prices and
   # the ask prices is always empty.
   test "apply delta to bids side", state do
-    [level | _] = state[:data]
+    [level | _] = state[:bids]
     OrderBook.apply_delta(state[:book], :bid, level)
     assert OrderBook.best_bid(state[:book]) == level
   end
 
   test "apply delta to ask side", state do
-    [level | _] = state[:data]
+    [level | _] = state[:asks]
     OrderBook.apply_delta(state[:book], :ask, level)
     assert OrderBook.best_ask(state[:book]) == level
   end
+
+  test "apply snapshot (asks and bids)", state do
+    book = state[:book]
+    bids = state[:bids]
+    asks = state[:asks]
+    
+    max_bid = Enum.max_by(bids, fn {x, _} -> x end)
+    min_ask = Enum.min_by(asks, fn {x, _} -> x end)
+
+    OrderBook.apply_snapshot(book, {bids, asks})
+    assert OrderBook.best_ask(book) == min_ask 
+       and OrderBook.best_bid(book) == max_bid
+  end
+
+  test "get_bids returns bid list in correct order", state do
+    book = state[:book]
+    bids = Enum.sort_by(state[:bids], fn {x, _} -> -x end)
+
+    OrderBook.apply_snapshot(book, {bids, []})
+    assert bids == OrderBook.bids(book)
+  end
+
+  test "get_asks returns ask list in correct order", state do
+    book = state[:book]
+    asks = Enum.sort_by(state[:asks], fn {x, _} -> x end)
+
+    OrderBook.apply_snapshot(book, {[], asks})
+    assert asks == OrderBook.asks(book)
+  end
+
+  test "book returns bid and ask sides, both correctly ordered", state do
+    book = state[:book]
+    bids = Enum.sort_by(state[:bids], fn {x, _} -> -x end)
+    asks = Enum.sort_by(state[:asks], fn {x, _} -> x end)
+
+    OrderBook.apply_snapshot(book, {bids, asks})
+    assert {^bids, ^asks} = OrderBook.book(book)
+  end
+
 end
