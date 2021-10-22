151f9dee10ef3eae63e521ec3ac11039de54d9a3
William Santos <william.santos@hotmail.co.uk>
2021-10-22 20:20:19 +0100

    update comments

--- test/orderbook_test.exs
+++ test/orderbook_test.exs
@@ -2,10 +2,6 @@ defmodule OrderBookTest do
   @moduledoc """
   Unit tests for the OrderBook module
 
-  All test functions will return :empty if the respective book side is empty.
-  Also, all test functions behave in respect to an orderbook Agent (require
-  first arg to be agent pid).
-
   ## Testing
   - best_bid       :: float                  # return best (highest) bid
   - best_ask       :: float                  # return best (lowest) ask
@@ -28,11 +24,17 @@ defmodule OrderBookTest do
 
   def rand_sample(n, range) do
     for(_ <- 1..n, do: Enum.random(range))
-    |> Enum.uniq
+    |> Enum.uniq()
   end
 
-  # not setup_all because by default mix will randomize the order in which tests are executed
-  # as such each test will have a fresh set of random sample deltas
+  # not setup_all because by default mix will randomize the order in which
+  # tests are executed as such each test will have a fresh set of random
+  # sample deltas.
+  #
+  # separate bids and asks, such that bid prices are in 0..999 and ask prices
+  # are in 1001..2000 (a £2 spread). volume is the same for both sides. this
+  # is more representative. the intersection of the bid and ask prices is
+  # always empty.
   setup do
     {:ok, book} = OrderBook.start_link()
 
@@ -61,10 +63,6 @@ defmodule OrderBookTest do
     assert OrderBook.best_ask(state[:book]) == :side_empty
   end
 
-  # maybe: have separate "data"s for bids and asks, such that bid prices are
-  # in 0..999 and ask prices are in 1001..2000 (a £2 spread). the volume can
-  # stay the same. more representative. the intersection of the bid prices and
-  # the ask prices is always empty.
   test "apply delta to bids side", state do
     [level | _] = state[:bids]
     OrderBook.apply_delta(state[:book], :bid, level)
@@ -81,13 +79,14 @@ defmodule OrderBookTest do
     book = state[:book]
     bids = state[:bids]
     asks = state[:asks]
-    
+
     max_bid = Enum.max_by(bids, fn {x, _} -> x end)
     min_ask = Enum.min_by(asks, fn {x, _} -> x end)
 
     OrderBook.apply_snapshot(book, {bids, asks})
-    assert OrderBook.best_ask(book) == min_ask 
-       and OrderBook.best_bid(book) == max_bid
+
+    assert OrderBook.best_ask(book) == min_ask and
+             OrderBook.best_bid(book) == max_bid
   end
 
   test "get_bids returns bid list in correct order", state do
@@ -114,5 +113,4 @@ defmodule OrderBookTest do
     OrderBook.apply_snapshot(book, {bids, asks})
     assert {^bids, ^asks} = OrderBook.book(book)
   end
-
 end
