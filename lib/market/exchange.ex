7fcfcc3cbdf2dce784c72197c1a17c1d4ebf26ae
William Santos <william.santos@hotmail.co.uk>
2021-11-03 10:46:54 +0000

    added api functions to genservers

--- lib/market/exchange.ex
+++ lib/market/exchange.ex
@@ -16,14 +16,16 @@ defmodule Market.Exchange do
   ...
   """
   @impl true
-  def handle_call(request, from, state) do
+  def handle_cast({:best_bid_change, new_price}, state) do
+    IO.puts("best bid change #{new_price}")
   end
 
   @doc """
   ...
   """
   @impl true
-  def handle_cast(request, state) do
+  def handle_cast({:best_ask_change, new_price}, state) do
+    IO.puts("best ask change #{new_price}")
   end
 
   @doc """
@@ -31,5 +33,19 @@ defmodule Market.Exchange do
   """
   @impl true
   def terminate(reason, state) do
+  end
+
+  @doc """
+  ...
+  """
+  def best_bid_change(exchange, new_price) do
+    GenServer.cast(exchange, {:best_bid_change, new_price})
+  end
+
+  @doc """
+  ...
+  """
+  def best_ask_change(exchange, new_price) do
+    GenServer.cast(exchange, {:best_ask_change, new_price})
   end
 end
--- lib/market/level2/mediator.ex
+++ lib/market/level2/mediator.ex
@@ -10,26 +10,61 @@ defmodule Market.Level2.Mediator do
   """
   @impl true
   def init(args) do
+    {:ok, orderbook} = Market.Level2.Orderbook.start_link()
+    {:ok, orderbook}
   end
 
   @doc """
   ...
   """
   @impl true
-  def handle_call(request, from, state) do
+  def handle_call({:snapshot, {bids, asks}}, from, orderbook) do
+    Market.Level2.Orderbook.apply_snapshot(orderbook, {bids, asks})
   end
 
   @doc """
   ...
   """
   @impl true
-  def handle_cast(request, state) do
+  def handle_call({:delta, {:bid, price, liquidity}}, from, orderbook) do
+    Market.Level2.Orderbook.apply_delta(orderbook, :bid, {price, liquidity})
   end
 
   @doc """
   ...
   """
   @impl true
-  def terminate(reason, state) do
+  def handle_call({:delta, {:ask, price, liquidity}}, from, orderbook) do
+    Market.Level2.Orderbook.apply_delta(orderbook, :ask, {price, liquidity})
+  end
+
+  @doc """
+  ...
+  """
+  @impl true
+  def terminate(reason, orderbook) do
+    # handle termination
+    # ...
+  end
+
+  @doc """
+  ...
+  """
+  def snapshot(mediator, bids, asks) do
+    GenServer.call(mediator, {:snapshot, {bids, asks}})
+  end
+
+  @doc """
+  ...
+  """
+  def delta(mediator, {:bid, price, liquidity}) do
+    GenServer.call(mediator, {:delta, {:bid, price, liquidity}})
+  end
+
+  @doc """
+  ...
+  """
+  def delta(mediator, {:ask, price, liquidity}) do
+    GenServer.call(mediator, {:delta, {:ask, price, liquidity}})
   end
 end
