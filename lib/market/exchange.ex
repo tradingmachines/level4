61e68b3c64532f314cf5341baf2e4f999c75fc87
William Santos <william.santos@hotmail.co.uk>
2021-11-14 16:42:03 +0000

    finish best price check

--- lib/market/exchange.ex
+++ lib/market/exchange.ex
@@ -39,16 +39,18 @@ defmodule Market.Exchange do
         "starting exchange"
     )
 
-    # TODO
+    # TODO setup storage medium
+    # ...
+    store = nil
 
-    {:ok, %{}}
+    {:ok, {init_arg[:market], store}}
   end
 
   @doc """
   Terminate function for the GenServer. Handles tear-down of the storage medium.
   """
   @impl true
-  def terminate(reason, state) do
+  def terminate(reason, {market, _}) do
     # handle termination
     # ...
   end
@@ -58,31 +60,47 @@ defmodule Market.Exchange do
   received from the mediator process.
   """
   @impl true
-  def handle_cast({:best_bid_change, new_price}, state) do
-    IO.puts("best bid change #{new_price}")
+  def handle_cast(
+        {:best_bid_change, {new_price, new_size}},
+        {market, store}
+      ) do
+    Logger.debug(
+      "#{Market.id(market)} " <>
+        "best bid change: #{new_price}"
+    )
 
     # TODO
+
+    {:noreply, {market, store}}
   end
 
-  def handle_cast({:best_ask_change, new_price}, state) do
-    IO.puts("best ask change #{new_price}")
+  def handle_cast(
+        {:best_ask_change, {new_price, new_size}},
+        {market, store}
+      ) do
+    Logger.debug(
+      "#{Market.id(market)} " <>
+        "best ask change: #{new_price}"
+    )
 
     # TODO
+
+    {:noreply, {market, store}}
   end
 
   @doc """
   Market.Exchange API: a helper function that sends a :best_bid_change to
   the GenServer. Note: casts are asynchronous requests.
   """
-  def best_bid_change(exchange, new_price) do
-    GenServer.cast(exchange, {:best_bid_change, new_price})
+  def best_bid_change(exchange, {new_price, new_size}) do
+    GenServer.cast(exchange, {:best_bid_change, {new_price, new_size}})
   end
 
   @doc """
   Market.Exchange API: a helper function that sends a :best_ask_change to
   the GenServer. Note: casts are asynchronous requests.
   """
-  def best_ask_change(exchange, new_price) do
-    GenServer.cast(exchange, {:best_ask_change, new_price})
+  def best_ask_change(exchange, {new_price, new_size}) do
+    GenServer.cast(exchange, {:best_ask_change, {new_price, new_size}})
   end
 end
--- lib/market/level2/mediator.ex
+++ lib/market/level2/mediator.ex
@@ -33,14 +33,14 @@ defmodule Market.Level2.Mediator do
         "starting mediator"
     )
 
-    {:ok, init_arg[:market]}
+    {:ok, {init_arg[:market], {{0, 0}, {0, 0}}}}
   end
 
   @doc """
   ...
   """
   @impl true
-  def handle_call({:snapshot, {bids, asks}}, _, market) do
+  def handle_call({:snapshot, {bids, asks}}, _, {market, best_prices}) do
     Market.Level2.OrderBook.apply_snapshot(
       {:via, Registry,
        {
@@ -50,28 +50,150 @@ defmodule Market.Level2.Mediator do
       {bids, asks}
     )
 
-    {:reply, %{}, market}
+    {:reply, %{}, {market, best_prices}}
   end
 
-  def handle_call({:delta, {side, price, liquidity}}, _, market) do
+  def handle_call(
+        {:delta, {:bid, price, liquidity}},
+        _,
+        {market, {{best_bid_price, best_bid_size}, best_ask}}
+      ) do
+    # apply the delta to the order book
     Market.Level2.OrderBook.apply_delta(
       {:via, Registry,
        {
          Market.Level2.OrderBook.Registry,
          Market.id(market)
        }},
-      side,
+      :bid,
       {price, liquidity}
     )
 
-    {:reply, %{}, market}
+    # get the current best bid price
+    case Market.Level2.OrderBook.best_bid(
+           {:via, Registry,
+            {
+              Market.Level2.OrderBook.Registry,
+              Market.id(market)
+            }}
+         ) do
+      # check if the best bid price changed
+      {new_best_bid_price, new_best_bid_size} ->
+        if best_bid_price != new_best_bid_price do
+          Market.Exchange.best_bid_change(
+            {:via, Registry,
+             {
+               Market.Exchange.Registry,
+               Market.id(market)
+             }},
+            {new_best_bid_price, new_best_bid_size}
+          )
+
+          {
+            :reply,
+            %{},
+            {
+              market,
+              {{new_best_bid_price, new_best_bid_size}, best_ask}
+            }
+          }
+        else
+          {
+            :reply,
+            %{},
+            {
+              market,
+              {{best_bid_price, best_bid_size}, best_ask}
+            }
+          }
+        end
+
+      # the bids side is empty
+      :side_empty ->
+        {
+          :reply,
+          %{},
+          {
+            market,
+            {{best_bid_price, best_bid_size}, best_ask}
+          }
+        }
+    end
+  end
+
+  def handle_call(
+        {:delta, {:ask, price, liquidity}},
+        _,
+        {market, {best_bid, {best_ask_price, best_ask_size}}}
+      ) do
+    # apply the delta to the order book
+    Market.Level2.OrderBook.apply_delta(
+      {:via, Registry,
+       {
+         Market.Level2.OrderBook.Registry,
+         Market.id(market)
+       }},
+      :ask,
+      {price, liquidity}
+    )
+
+    # get the current best ask price
+    case Market.Level2.OrderBook.best_ask(
+           {:via, Registry,
+            {
+              Market.Level2.OrderBook.Registry,
+              Market.id(market)
+            }}
+         ) do
+      # check if the best ask price changed
+      {new_best_ask_price, new_best_ask_size} ->
+        if best_ask_price != new_best_ask_price do
+          Market.Exchange.best_ask_change(
+            {:via, Registry,
+             {
+               Market.Exchange.Registry,
+               Market.id(market)
+             }},
+            {new_best_ask_price, new_best_ask_size}
+          )
+
+          {
+            :reply,
+            %{},
+            {
+              market,
+              {best_bid, {new_best_ask_price, new_best_ask_size}}
+            }
+          }
+        else
+          {
+            :reply,
+            %{},
+            {
+              market,
+              {best_bid, {best_ask_price, best_ask_size}}
+            }
+          }
+        end
+
+      # the asks side is empty
+      :side_empty ->
+        {
+          :reply,
+          %{},
+          {
+            market,
+            {best_bid, {best_ask_price, best_ask_size}}
+          }
+        }
+    end
   end
 
   @doc """
   ...
   """
   @impl true
-  def terminate(reason, market) do
+  def terminate(reason, {market, _}) do
     Logger.info(
       "#{Market.id(market)} shutdown " <>
         "mediator: #{reason}"
