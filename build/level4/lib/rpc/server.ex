c7abb8f846a6d4974b9500b8af44ca88ab7fbe6d
william santos <w@wsantos.net>
2023-02-23 05:54:36 +0000

    simplify code

--- build/level4/lib/rpc/server.ex
+++ build/level4/lib/rpc/server.ex
@@ -20,114 +20,63 @@ defmodule Level4.RPC.Server do
           Level4.StartMarketRequest.t(),
           GRPC.Server.Stream.t()
         ) :: Level4.StartMarketReply.t()
-  def start_market(request, _stream) do
-    # ...
-    market = %Market{
-      id: request.market.id,
-      base_symbol: request.market.base_symbol,
-      quote_symbol: request.market.quote_symbol,
-      exchange_name: request.market.exchange_name,
-      type: request.market.type
-    }
+  def start_market(request, _stream),
+    do: case(Level4.start_market(request.market)) do
+    {:error, reason} ->
+      # ...
+      raise GRPC.RPCError, status: GRPC.Status.unknown(), message: reason
 
-    case Level4.start_market(market) do
-      {:error, reason} ->
-        # ...
-        raise GRPC.RPCError, status: GRPC.Status.unknown(), message: reason
-
-      {:ok, node} ->
-        # ...
-        Level4.RPC.Server.StartMarketReply.new(
-          market:
-            Level4.RPC.Server.Market.new(
-              id: market.id,
-              base_symbol: market.base_symbol,
-              quote_symbol: market.quote_symbol,
-              exchange_name: market.exchange_name,
-              type: market.type
-            ),
-          node:
-            Level4.RPC.Server.Node.new(
-              name: "#{node}",
-              active_market_count: 0
-            )
-        )
-    end
+    {:ok, node} ->
+      # ...
+      Level4.RPC.Server.StartMarketReply.new(
+        market: request.market |> Map.to_list() |> Level4.RPC.Server.Market.new(),
+        node: Level4.RPC.Server.Node.new(name: "#{node}", active_market_count: 0)
+      )
   end
 
   @spec stop_market(
           Level4.StopMarketRequest.t(),
           GRPC.Server.Stream.t()
         ) :: Level4.StopMarketReply.t()
-  def stop_market(request, _stream) do
-    # ...
-    market = %Market{
-      id: request.market.id,
-      base_symbol: request.market.base_symbol,
-      quote_symbol: request.market.quote_symbol,
-      exchange_name: request.market.exchange_name,
-      type: request.market.type
-    }
+  def stop_market(request, _stream),
+    do: case(Level4.stop_market(request.market)) do
+    {:error, reason} ->
+      # ...
+      raise GRPC.RPCError, status: GRPC.Status.unknown(), message: reason
 
-    case Level4.stop_market(market) do
-      {:error, reason} ->
-        # ...
-        raise GRPC.RPCError, status: GRPC.Status.unknown(), message: reason
-
-      {:ok, node} ->
-        # ...
-        Level4.RPC.Server.StopMarketReply.new(
-          market:
-            Level4.RPC.Server.Market.new(
-              id: market.id,
-              base_symbol: market.base_symbol,
-              quote_symbol: market.quote_symbol,
-              exchange_name: market.exchange_name,
-              type: market.type
-            ),
-          node:
-            Level4.RPC.Server.Node.new(
-              name: "#{node}",
-              active_market_count: 0
-            )
-        )
-    end
+    {:ok, node} ->
+      # ...
+      Level4.RPC.Server.StopMarketReply.new(
+        market: request.market |> Map.to_list() |> Level4.RPC.Server.Market.new(),
+        node: Level4.RPC.Server.Node.new(name: "#{node}", active_market_count: 0)
+      )
   end
 
   @spec stop_market(
           Level4.ListNodesRequest.t(),
           GRPC.Server.Stream.t()
         ) :: Level4.ListNodesReply.t()
-  def list_nodes(request, _stream) do
-    Level4.RPC.Server.ListNodesReply.new(
-      nodes:
-        Level4.list_nodes(:all)
-        |> Enum.map(fn x ->
-          Level4.RPC.Server.Node.new(
-            name: "#{x}",
-            active_market_count: 0
-          )
-        end)
-    )
-  end
+  def list_nodes(request, _stream),
+    do:
+      Level4.RPC.Server.ListNodesReply.new(
+        nodes:
+          Level4.list_nodes(:all)
+          |> Enum.map(fn x ->
+            Level4.RPC.Server.Node.new(name: "#{x}", active_market_count: 0)
+          end)
+      )
 
   @spec list_active_markets(
           ListMarketsRequest.t(),
           GRPC.Server.Stream.t()
         ) :: Level4.ListMarketsReply.t()
-  def list_active_markets(request, _stream) do
-    Level4.RPC.Server.ListMarketsReply.new(
-      markets:
-        Level4.list_active_markets(:all)
-        |> Enum.map(fn x ->
-          Level4.RPC.Server.Market.new(
-            id: x.id,
-            base_symbol: x.base_symbol,
-            quote_symbol: x.quote_symbol,
-            exchange_name: x.exchange_name,
-            type: x.type
-          )
-        end)
-    )
-  end
+  def list_active_markets(request, _stream),
+    do:
+      Level4.RPC.Server.ListMarketsReply.new(
+        markets:
+          Level4.list_active_markets(:all)
+          |> Enum.map(fn x ->
+            x |> Map.to_list() |> Level4.RPC.Server.Market.new()
+          end)
+      )
 end
