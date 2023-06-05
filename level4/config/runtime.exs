8774513e2baeb68357c1e9d0c76115ade5032698
william santos <w@wsantos.net>
2023-06-05 21:12:57 +0100

    fix environment variables

--- level4/config/runtime.exs
+++ level4/config/runtime.exs
@@ -1,16 +1,49 @@
 import Config
 
+def get_env(:atom, name, default) do
+  System.get_env(name, default) |> String.to_atom()
+end
+
+def get_env(:integer, name, default) do
+  {x, ""} = System.get_env(name, default) |> Integer.parse()
+  x
+end
+
+def get_env(:hosts, name, default) do
+  System.get_env(name, default)
+  |> String.split(",")
+  |> Enum.map(fn addr ->
+    [ip_address, port] = String.split(addr, ":")
+    %{ip_address => String.to_integer(port)}
+  end)
+end
+
 # define the node hostname
 config :level4,
-  hostname: System.get_env("HOSTNAME", "node1") |> String.to_atom()
+  hostname: get_env(:atom, "HOSTNAME", "node1")
 
 # define the maximum concurrent data feed processes per node
 config :level4,
-  max_data_feeds: 25
+  max_data_feeds: get_env(:integer, "MAX_DATA_FEEDS", "25")
 
 # define the RPC server port
 config :level4,
-  rpc_port: 50051
+  rpc_port: get_env(:integer, "RPC_PORT", "50051")
+
+# define the kafka producer
+config :kaffe,
+  producer: [
+    endpoints: get_env(:hosts, "KAFKA_ENDPOINTS", ["127.0.0.1:9093"]),
+    topics: [
+      "level4.spread",
+      "level4.timesale"
+    ],
+    partition_strategy: fn _topic, _partitions_count, key, _value ->
+      # subtract 1 because topics start at zero
+      {market_id, ""} = Integer.parse(key)
+      market_id - 1
+    end
+  ]
 
 # define the libcluster topologies
 config :level4,
@@ -22,22 +55,4 @@ config :level4,
         multicast_ttl: 1
       ]
     ]
-  ]
-
-# define the kafka producer
-config :kaffe,
-  producer: [
-    endpoints: [
-      "127.0.0.1": 9093
-    ],
-    topics: [
-      "level4.spread",
-      "level4.timesale"
-    ],
-    partition_strategy: fn _topic, _partitions_count, key, _value ->
-      {market_id, ""} = Integer.parse(key)
-
-      # subtract 1 because topics start at zero
-      market_id - 1
-    end
   ]
