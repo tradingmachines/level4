5a35aa7b6e9c7403b8f6d0208c94ea09f0cee16e
william santos <w@wsantos.net>
2023-06-09 19:54:24 +0100

    fixed price check error

--- level4/lib/market/datafeed/mediator.ex
+++ level4/lib/market/datafeed/mediator.ex
@@ -29,9 +29,18 @@ defmodule Market.DataFeed.Level2.Mediator do
        %{
          :market => init_arg[:market],
          :node => init_arg[:node],
-         :best_bid => best_price(0, 0),
-         :best_ask => best_price(0, 0)
+         :best_bid => price_level(:default),
+         :best_ask => price_level(:default)
        }}
+
+  @doc """
+  Return map containg price and liquidity.
+  """
+  def price_level(price, liquidity),
+    do: %{:price => price, :liquidity => liquidity}
+
+  def price_level(:default),
+    do: %{:price => 0, :liquidity => 0}
 
   @doc """
   Do comparison between two prices.
@@ -40,23 +49,12 @@ defmodule Market.DataFeed.Level2.Mediator do
     do: best != current
 
   @doc """
-  Return map containg price and liquidity.
-  """
-  def best_price(price, liquidity),
-    do: %{:price => price, :liquidity => liquidity}
-
-  @doc """
   Handle snapshot / deltas / and buys/sells.
   """
   @impl true
   def handle_call({:snapshot, bid_levels, ask_levels}, _, state) do
     # get the orderbook pid
-    book =
-      {:via, Registry,
-       {
-         Market.OrderBook.Registry,
-         state[:market].id
-       }}
+    book = {:via, Registry, {Market.OrderBook.Registry, state[:market].id}}
 
     # apply the snapshot
     :ok =
@@ -65,26 +63,32 @@ defmodule Market.DataFeed.Level2.Mediator do
         {bid_levels, ask_levels}
       )
 
-    {:reply, :ok, state}
+    # get the new best bid
+    {
+      best_bid_price,
+      best_bid_liquidity
+    } = Market.DataFeed.Level2.OrderBook.best_bid(book)
+
+    # get the new best ask
+    {
+      best_ask_price,
+      best_ask_liquidity
+    } = Market.DataFeed.Level2.OrderBook.best_ask(book)
+
+    # update state's best bid/ask levels
+    {:reply, :ok,
+     %{
+       state
+       | :best_bid => price_level(best_bid_price, best_bid_liquidity),
+         :best_ask => price_level(best_ask_price, best_ask_liquidity)
+     }}
   end
 
   @impl true
   def handle_call({:delta, side, price, liquidity}, _, state) do
-    # get the orderbook pid
-    book =
-      {:via, Registry,
-       {
-         Market.OrderBook.Registry,
-         state[:market].id
-       }}
-
-    # get the data feed pid
-    data_feed =
-      {:via, Registry,
-       {
-         Market.DataFeed.Registry,
-         state[:market].id
-       }}
+    # get the orderbook and datafeed pids
+    book = {:via, Registry, {Market.OrderBook.Registry, state[:market].id}}
+    data_feed = {:via, Registry, {Market.DataFeed.Registry, state[:market].id}}
 
     # apply the delta
     :ok =
@@ -94,24 +98,58 @@ defmodule Market.DataFeed.Level2.Mediator do
         {price, liquidity}
       )
 
+    # get the new best bid
+    {
+      best_bid_price,
+      best_bid_liquidity
+    } = Market.DataFeed.Level2.OrderBook.best_bid(book)
+
+    # get the new best ask
+    {
+      best_ask_price,
+      best_ask_liquidity
+    } = Market.DataFeed.Level2.OrderBook.best_ask(book)
+
     case side do
       :bid ->
-        if best_price_changed?(state[:best_bid][:price], price) do
-          # update best bid price
-          :ok = Market.DataFeed.best_bid_change(data_feed, price, liquidity)
-          {:reply, :ok, %{state | :best_bid => best_price(price, liquidity)}}
+        if best_price_changed?(state[:best_bid][:price], best_bid_price) do
+          # send a new event to the data feed
+          :ok =
+            Market.DataFeed.best_bid_change(
+              data_feed,
+              best_bid_price,
+              best_bid_liquidity
+            )
+
+          # update the state's best bid
+          {:reply, :ok,
+           %{
+             state
+             | :best_bid => price_level(best_bid_price, best_bid_liquidity)
+           }}
         else
-          # no need to update best bid price
+          # no need to update best bid
           {:reply, :ok, state}
         end
 
       :ask ->
-        if best_price_changed?(state[:best_ask][:price], price) do
-          # update best ask price
-          :ok = Market.DataFeed.best_ask_change(data_feed, price, liquidity)
-          {:reply, :ok, %{state | :best_ask => best_price(price, liquidity)}}
+        if best_price_changed?(state[:best_ask][:price], best_ask_price) do
+          # send a new event to the data feed
+          :ok =
+            Market.DataFeed.best_ask_change(
+              data_feed,
+              best_ask_price,
+              best_ask_liquidity
+            )
+
+          # update the state's best ask
+          {:reply, :ok,
+           %{
+             state
+             | :best_ask => price_level(best_ask_price, best_ask_liquidity)
+           }}
         else
-          # no need to update best ask price
+          # no need to update best ask
           {:reply, :ok, state}
         end
     end
@@ -120,20 +158,14 @@ defmodule Market.DataFeed.Level2.Mediator do
   @impl true
   def handle_call({:fill, side, price, size}, _, state) do
     # get the data feed pid
-    data_feed =
-      {:via, Registry,
-       {
-         Market.DataFeed.Registry,
-         state[:market].id
-       }}
+    data_feed = {:via, Registry, {Market.DataFeed.Registry, state[:market].id}}
 
+    # notify the data feed
     case side do
       :buy ->
-        # notify the data feed of the buy event
         :ok = Market.DataFeed.buy(data_feed, price, size)
 
       :sell ->
-        # notify the data feed of the sell event
         :ok = Market.DataFeed.sell(data_feed, price, size)
     end
 
@@ -143,14 +175,9 @@ defmodule Market.DataFeed.Level2.Mediator do
   @impl true
   def handle_call({:status_update, event}, _, state) do
     # get the data feed pid
-    data_feed =
-      {:via, Registry,
-       {
-         Market.DataFeed.Registry,
-         state[:market].id
-       }}
+    data_feed = {:via, Registry, {Market.DataFeed.Registry, state[:market].id}}
 
-    # notify the data feed of status change
+    # notify the data feed
     :ok =
       Market.DataFeed.status_update(
         data_feed,
